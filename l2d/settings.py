c2237f5ea5439ccef3d0c00f71b91740784e1e37
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-04-18 18:37:37 +0100

    Fix import order and remove unused sys import in settings

--- l2d/settings.py
+++ l2d/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import cloudinary
 import os
-import sys
 import dj_database_url
+import cloudinary
 if os.path.isfile('env.py'):
     import env
 
